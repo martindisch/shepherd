0c5454cdc282df51f8915b9231c02f20084f3579
Martin Disch <martindisch@gmail.com>
2019-11-20 18:21:07 +0100

    Implement encoding

--- src/remote.rs
+++ src/remote.rs
@@ -1,9 +1,22 @@
 use crossbeam::channel::{self, Receiver};
-use std::{path::PathBuf, thread, time::Duration};
+use std::{path::PathBuf, process::Command, thread};
+
+static TMP_DIR: &str = "shepherd_tmp_remote";
 
 /// The parent thread managing the operations for a host.
 pub fn host_thread(host: String, global_receiver: Receiver<PathBuf>) {
     println!("Spawned host thread {}", host);
+
+    // Create temporary directory on host
+    let output = Command::new("ssh")
+        .args(&[&host, "mkdir", TMP_DIR])
+        .output()
+        .expect("Failed executing ssh command");
+    assert!(
+        output.status.success(),
+        "Failed creating remote temporary directory"
+    );
+
     // Create a channel holding a single chunk at a time for the encoder thread
     let (sender, receiver) = channel::bounded(1);
     // Create copy of host for thread
@@ -16,7 +29,15 @@ pub fn host_thread(host: String, global_receiver: Receiver<PathBuf>) {
     // Try to fetch a chunk from the global channel
     while let Ok(chunk) = global_receiver.recv() {
         println!("Host thread {} received chunk {:?}", host, chunk);
-        // TODO: transfer the chunk
+        // Transfer chunk to host
+        let output = Command::new("scp")
+            .args(&[
+                chunk.to_str().expect("Invalid Unicode"),
+                &format!("{}:{}", host, TMP_DIR),
+            ])
+            .output()
+            .expect("Failed executing scp command");
+        assert!(output.status.success(), "Failed transferring chunk");
         // Pass the chunk to the encoder thread (blocks until channel empty)
         sender.send(chunk).expect("Failed sending chunk in channel");
     }
@@ -26,9 +47,17 @@ pub fn host_thread(host: String, global_receiver: Receiver<PathBuf>) {
     println!("Host thread {} waiting for encoder to finish", host);
 
     // Wait for the encoder
-    handle
-        .join()
-        .expect("Encoder thread panicked");
+    handle.join().expect("Encoder thread panicked");
+
+    // Clean up temporary directory on host
+    let output = Command::new("ssh")
+        .args(&[&host, "rm", "-r", TMP_DIR])
+        .output()
+        .expect("Failed executing ssh command");
+    assert!(
+        output.status.success(),
+        "Failed removing remote temporary directory"
+    );
     println!("Host thread {} exiting", host);
 }
 
@@ -36,8 +65,52 @@ pub fn host_thread(host: String, global_receiver: Receiver<PathBuf>) {
 pub fn encoder_thread(host: String, receiver: Receiver<PathBuf>) {
     while let Ok(chunk) = receiver.recv() {
         println!("Encoder thread {} received chunk {:?}", host, chunk);
-        // TODO: encode chunk
-        thread::sleep(Duration::from_secs(2));
+        // Construct the chunk's remote file name
+        let chunk_name = format!(
+            "{}/{}",
+            TMP_DIR,
+            chunk
+                .file_name()
+                .expect("No normal file")
+                .to_str()
+                .expect("Invalid Unicode")
+        );
+        // Construct the encoded chunk's remote file name
+        let enc_name = format!(
+            "{}/{}.{}",
+            TMP_DIR,
+            chunk
+                .file_stem()
+                .expect("No normal file")
+                .to_str()
+                .expect("Invalid Unicode"),
+            "mp4"
+        );
+
+        // Encode the chunk remotely
+        let output = Command::new("ssh")
+            .args(&[
+                &host,
+                "ffmpeg",
+                "-i",
+                &chunk_name,
+                "-c:v",
+                "libx264",
+                "-crf",
+                "22",
+                "-preset",
+                "veryslow",
+                "-profile:v",
+                "high",
+                "-level",
+                "4.2",
+                "-pix_fmt",
+                "yuv420p",
+                &enc_name,
+            ])
+            .output()
+            .expect("Failed executing ssh command");
+        assert!(output.status.success(), "Failed encoding");
     }
     println!("Encoder thread {} exiting", host);
 }
