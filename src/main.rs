84a66589c9bf05f9387110e0dbec5161b92cf677
Martin Disch <martindisch@gmail.com>
2019-11-20 10:34:30 +0100

    Collect arguments we need for now

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .about(
+            "A distributed video encoder that splits files into chunks \
+             for multiple machines.",
+        )
+        .author(clap::crate_authors!())
+        .arg(
+            Arg::with_name("clients")
+                .short("c")
+                .long("clients")
+                .value_name("HOSTNAMES")
+                .takes_value(true)
+                .required(true)
+                .help("Comma-separated list of encoding hosts"),
+        )
+        .arg(
+            Arg::with_name("IN")
+                .help("The original video file")
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("OUT")
+                .help("The output video file")
+                .required(true),
+        )
+        .get_matches();
+    // If we get here, we know the arguments were supplied, so unwrap is safe
+    let input = matches.value_of("IN").unwrap();
+    let output = matches.value_of("OUT").unwrap();
+    let hosts = matches.value_of("clients").unwrap().split(',');
 }
